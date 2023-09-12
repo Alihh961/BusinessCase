551184c12a8a5d33bc7cb51df4fbdbf7baa27bc5
Alihh961 <hajhassan.ali@outlook.com>
2023-09-12 04:22:45 +0200

    adding Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,18 +9,18 @@ import {LoginComponent} from "./login/login.component";
 import {Error404Component} from "./error404/error404.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {ProfileComponent} from "./profile/profile.component";
+import {AuthGuard} from "./guard/auth.guard";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent, pathMatch: 'full'},
-  {path: 'nfts', component: NftsComponent, pathMatch: 'full'},
-  {path: 'nft/:i', component: ShownftComponent, pathMatch: 'full'},
-  {path: 'nft', component: ShownftComponent, pathMatch: 'full'},
-  {path: 'ethchart', component: EthchartComponent, pathMatch: 'full'},
-  {path: 'contact', component: ContactComponent, pathMatch: 'full'},
+  {path: 'nfts', component: NftsComponent, pathMatch: 'full',canActivate: [AuthGuard]},
+  {path: 'nft/:i', component: ShownftComponent, pathMatch: 'full',canActivate: [AuthGuard]},
+  {path: 'ethchart', component: EthchartComponent, pathMatch: 'full',canActivate: [AuthGuard]},
+  {path: 'contact', component: ContactComponent, pathMatch: 'full',canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent, pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent, pathMatch: 'full'},
-  {path: 'profile', component: ProfileComponent, pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent, pathMatch: 'full',canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, pathMatch: 'full' ,canActivate: [AuthGuard]},
   {path: '**', component: Error404Component, pathMatch: 'full'},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,72 +1,80 @@
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { ContactComponent } from './contact/contact.component';
-import { BanniereHomePageComponent } from './banniere-home-page/banniere-home-page.component';
-import { CookiesNotificationComponent } from './cookies-notification/cookies-notification.component';
-import { Error404Component } from './error404/error404.component';
-import { FilterComponent } from './filter/filter.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { SearchComponent } from './search/search.component';
-import { NftsComponent } from './nfts/nfts.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
-import { CookieService } from 'ngx-cookie-service';
+import {AppComponent} from './app.component';
+import {ContactComponent} from './contact/contact.component';
+import {BanniereHomePageComponent} from './banniere-home-page/banniere-home-page.component';
+import {CookiesNotificationComponent} from './cookies-notification/cookies-notification.component';
+import {Error404Component} from './error404/error404.component';
+import {FilterComponent} from './filter/filter.component';
+import {GalleryComponent} from './gallery/gallery.component';
+import {SearchComponent} from './search/search.component';
+import {NftsComponent} from './nfts/nfts.component';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {LoginComponent} from './login/login.component';
+import {CookieService} from 'ngx-cookie-service';
 import {Console, DateFormatPipe} from './pipes/date-transform.pipe';
-import { ShownftComponent } from './shownft/shownft.component';
-import { EthchartComponent } from './ethchart/ethchart.component';
+import {ShownftComponent} from './shownft/shownft.component';
+import {EthchartComponent} from './ethchart/ethchart.component';
 import {HttpinterceptorInterceptor} from "./services/httpinterceptor/httpinterceptor.interceptor";
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProfileComponent } from './profile/profile.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ProfileComponent} from './profile/profile.component';
+import {AuthGuard} from "./guard/auth.guard";
 
-const appRoutes: Routes = [
-  ];
+const appRoutes: Routes = [];
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ContactComponent,
-        BanniereHomePageComponent,
-        CookiesNotificationComponent,
-        Error404Component,
-        FilterComponent,
-        GalleryComponent,
-        SearchComponent,
-        NftsComponent,
-        HomeComponent,
-        HeaderComponent,
-        FooterComponent,
-        LoginComponent,
-        DateFormatPipe,
-        Console,
-        ShownftComponent,
-        EthchartComponent,
-        DashboardComponent,
-        ProfileComponent,
-    ],
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    BanniereHomePageComponent,
+    CookiesNotificationComponent,
+    Error404Component,
+    FilterComponent,
+    GalleryComponent,
+    SearchComponent,
+    NftsComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    DateFormatPipe,
+    Console,
+    ShownftComponent,
+    EthchartComponent,
+    DashboardComponent,
+    ProfileComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes, { useHash: false })
+    RouterModule.forRoot(appRoutes, {
+      useHash: false,
+
+    })
   ],
   providers: [
     CookieService,
+    AuthGuard
     // {
     //   provide: HTTP_INTERCEPTORS,
     //   useClass: HttpinterceptorInterceptor,
     //   multi: true,
     // },
   ],
+  exports: [
+    RouterModule
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
